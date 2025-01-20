631707a50fee6cfe44bd872fd810fec247285816
r4mpage4 <155855664+r4mpage4@users.noreply.github.com>
2025-01-20 15:35:34 +0800

    Delete muchloverey.lua

--- muchloverey.lua
+++
@@ -1,6 +0,0 @@
-local library = {count = 0};
-
-local FindLibrary = game:GetService("CoreGui"):FindFirstChild("UI Library")
-if game:GetService("CoreGui"):FindFirstChild("UI Library") then
-    game:GetService("CoreGui"):FindFirstChild("UI Library"):Destroy()
-end
